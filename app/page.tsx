ab36c16ff08034dff5292486efc03df8a5bd99c1
Jason-1 <jason722698@gmail.com>
2025-02-24 10:49:26 +1300

    Added Footer

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Experience from "@/components/Experience";
+import Footer from "@/components/Footer";
 import Grid from "@/components/grid";
 import Hero from "@/components/Hero";
 import RecentProjects from "@/components/RecentProjects";
@@ -14,6 +15,7 @@ export default function Home() {
         <Grid />
         <RecentProjects />
         <Experience />
+        <Footer />
       </div>
     </main>
   );
