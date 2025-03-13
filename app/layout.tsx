cd53b4279659895ff0bee72e806ad8747244b991
Jason-1 <jason722698@gmail.com>
2025-03-13 13:24:41 +1300

    favicon and description

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,7 @@ const inter = Inter({
 
 export const metadata: Metadata = {
   title: "Jason's Portfolio",
-  description: "TODO add description",
+  description: "Welcome to my developer portfolio",
 };
 
 export default function RootLayout({
