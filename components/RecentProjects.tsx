c7f9169363382b312064cde075af68059361be62
Jason-1 <jason722698@gmail.com>
2025-03-14 10:26:34 +1300

    Removed unused imports

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -1,6 +1,5 @@
 import { projects } from "@/data";
 import React from "react";
-import { PinContainer } from "./ui/3d-pin";
 import { FaLocationArrow } from "react-icons/fa";
 
 const RecentProjects = () => {
